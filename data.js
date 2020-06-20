eeaa34bde6ef9f5d95e14ffbdc7669c7a710f126
Dudy Ali <dudyali@gmail.com>
2020-06-20 13:50:50 +0700

    initial commit

---
+++ data.js
@@ -0,0 +1,161 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-dudy-ali---information",
+      "name": "Dudy Ali - Information",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1440,
+      "initialViewParameters": {
+        "yaw": -0.08885312555606895,
+        "pitch": 0.03320041752802538,
+        "fov": 1.5521195194347879
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.7494679031509843,
+          "pitch": 0.022073344436078557,
+          "rotation": 0,
+          "target": "3-dudy-ali---lifestyle--sa"
+        },
+        {
+          "yaw": 0.6406845258047635,
+          "pitch": 0.058038896466928946,
+          "rotation": 0,
+          "target": "2-dudy-ali---ratc"
+        },
+        {
+          "yaw": 1.6660241895245775,
+          "pitch": 0.10163193389357161,
+          "rotation": 0,
+          "target": "1-dudy-ali---outdoor-gate"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -0.43202239752891103,
+          "pitch": -0.16419976875025455,
+          "title": "Prototyping 360 View",
+          "text": "Welcome to BMW"
+        }
+      ]
+    },
+    {
+      "id": "1-dudy-ali---outdoor-gate",
+      "name": "Dudy Ali - Outdoor Gate",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1440,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [],
+      "infoHotspots": []
+    },
+    {
+      "id": "2-dudy-ali---ratc",
+      "name": "Dudy Ali - RATC",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1440,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [],
+      "infoHotspots": []
+    },
+    {
+      "id": "3-dudy-ali---lifestyle--sa",
+      "name": "Dudy Ali - Lifestyle & SA",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1440,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [],
+      "infoHotspots": []
+    }
+  ],
+  "name": "Prabu 360",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": false,
+    "fullscreenButton": true,
+    "viewControlButtons": true
+  }
+};
